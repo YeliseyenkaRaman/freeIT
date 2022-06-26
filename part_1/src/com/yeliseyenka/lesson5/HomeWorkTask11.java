edc3e9a49df99db9cd1be35d573f8a6b3a1c4fd4
Raman Yeliseyenka <raman.yeliseyenka@gmail.com>
2022-06-26 22:58:18 +0300

    Lesson 5: Task11 Calculation of the factorial of a number

---
+++ part_1/src/com/yeliseyenka/lesson5/HomeWorkTask11.java
@@ -0,0 +1,22 @@
+package com.yeliseyenka.lesson5;
+
+/**
+ * «адание:
+ * ѕосчитать факториал числа в границах от 10 до 15 (не использу€ рекурсию).
+ */
+
+public class HomeWorkTask11 {
+    public static void main(String[] args) {
+
+        long factorialN = 1;
+
+        for (int i = 1; i <= 15; i++) {
+
+            factorialN *= i;
+
+            if (i >= 10) {
+                System.out.println("The factorial of the number " + i + " is equal to: " + factorialN);
+            }
+        }
+    }
+}
